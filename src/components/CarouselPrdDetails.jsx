ba1a73d9a094b5ee107aa31ac16fe2f6a106c770
lioilucas75gmail.com <lioilucas75@gmail.com>
2023-07-19 01:04:49 -0300

    updates

--- src/components/CarouselPrdDetails.jsx
+++ src/components/CarouselPrdDetails.jsx
@@ -7,7 +7,7 @@ const CarouselPrdDetails = ({data}) => {
   <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
   <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
   <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 4"></button>
+  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
 </div>
 <div className="carousel-inner">
   <div className="carousel-item active">
